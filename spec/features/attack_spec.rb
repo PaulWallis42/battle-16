70181bda7b312f9fb1774a8cf94e20d72410350b
paulwallis42 <paulwallis42@gmail.com>
2016-04-27 12:42:18 +0100

    player class added

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,4 +8,12 @@ feature 'Attacking' do
     expect(page).to have_content 'Dave attacked Mittens'
   end
 
+  scenario 'As player 1 I want an attack to reduce oppenents HP' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Mittens 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,11 +4,37 @@ require 'spec_helper'
 describe Player do
 
   subject(:dave) { Player.new('Dave') }
+  subject(:mittens) { Player.new('Mittens') }
 
   describe '#name' do
 
     it 'returns the name' do
       expect(dave.name).to eq 'Dave'
+    end
+
+  end
+
+  describe '#hit_points' do
+
+    it 'keeps a balance of hit points' do
+      expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+
+  end
+
+  describe '#attack' do
+
+    it 'damages the player' do
+      expect(mittens).to receive(:receive_damage)
+      dave.attack(mittens)
+    end
+
+  end
+
+  describe '#receive damage' do
+
+    it 'deducts from hit points' do
+      expect { dave.receive_damage }.to change { dave.hit_points }.by(-10)
     end
 
   end

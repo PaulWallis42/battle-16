d83acad4a2567d8f1c51a32d3396378c53dfef72
paulwallis42 <paulwallis42@gmail.com>
2016-04-27 10:14:44 +0100

    web_helper created

--- app.rb
+++ app.rb
@@ -2,13 +2,23 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    @player_1_hits = 60
+    @player_2_hits = 60
     erb :play
   end
 
---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'Show hit points' do
+
+  scenario 'I want to see my opponents hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Mittens: 60HP'
+  end
+
+end

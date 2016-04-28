17d241bac87b6d310009b6231b3d4aad650d6d75
paulwallis42 <paulwallis42@gmail.com>
2016-04-28 16:59:11 +0100

    class methods

--- lib/game.rb
+++ lib/game.rb
@@ -9,6 +9,14 @@ class Game
     @turn = player_1
   end
 
+  def self.create(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+
+  def self.instance
+    @game
+  end
+
   def player_1
     @players.first
   end
